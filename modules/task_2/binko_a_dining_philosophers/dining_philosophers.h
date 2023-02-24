9c687c934d0c093d3bb157bf4030f331076d9a4b
Vladimir Elistratov <aleksandrvlad63@gmail.com>
2023-02-24 18:16:52 +0300

    first commit

---
+++ modules/task_2/binko_a_dining_philosophers/dining_philosophers.h
@@ -0,0 +1,12 @@
+// Copyright 2023 Binko Alexander
+#ifndef MODULES_TASK_2_BINKO_A_DINING_PHILOSOPHERS_DINING_PHILOSOPHERS_H_
+#define MODULES_TASK_2_BINKO_A_DINING_PHILOSOPHERS_DINING_PHILOSOPHERS_H_
+
+#include <mpi.h>
+
+#include <cinttypes>
+#include <utility>
+
+void dinning(int64_t* data);
+
+#endif  //  MODULES_TASK_2_BINKO_A_DINING_PHILOSOPHERS_DINING_PHILOSOPHERS_H_
